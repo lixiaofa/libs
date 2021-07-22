73e4a760ef7afd8de3dae228cd5a9576e782fba7
lixiaofa <li_xf@hisuntech.com>
2021-07-22 17:02:46 +0800

    docs(exampleScope): 文档修改

--- packages/plugin-auth-util/src/index.ts
+++ packages/plugin-auth-util/src/index.ts
@@ -2,7 +2,7 @@
  * @Descripttion: 
  * @Date: 2021-06-23 10:29:38
  * @LastEditors: 司马老贼
- * @LastEditTime: 2021-07-22 16:38:25
+ * @LastEditTime: 2021-07-22 16:58:44
  * @Author: Sima thief
  */
 
@@ -10,17 +10,17 @@
 
 
 import Cookies from 'js-cookie'
-const TOKEN = 'token'
+const Token = 'Token'
 
 
 export function getToken() {
-    return Cookies.get(TOKEN)
+    return Cookies.get(Token)
 }
 export function setToken(token: string) {
-    return Cookies.set(TOKEN, token)
+    return Cookies.set(Token, token)
 }
 export function removeToken() {
-    return Cookies.remove(TOKEN)
+    return Cookies.remove(Token)
 }
 
 
